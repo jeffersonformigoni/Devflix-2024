813fd4dcd8f3afcd5942f5b65061bce16bd085b1
Jefferson Formigoni <jeffersonformigoni86@gmail.com>
2024-10-24 12:38:13 -0300

    Audio tundun adicionado e finalização da page

--- scripts.js
+++ scripts.js
@@ -1,43 +1,54 @@
-/*
-    JavaScript (E qualquer linguagem)
-        - Variável
-    Um pedacinho da memória do computador,
-    que eu posso guardar o que eu quiser
+/*   
+JavaScript (E qualquer linguagem)
+    - Variável 
+Um pedacinho da memória do computador, 
+que eu posso guardar o que eu quiser
+
+    - Funções 
+Pedacinho de código que só executa,
+Quando é chamado
 
 
-
-    - Funções (NÃO é uma função MATEMÁTICA)
-    - Pedacinho de código que só executa quando é chamado
-
-    Botão -> For clicado -> Chamar a minha Função -> Ligar o SOM
-    Document = HTML
-    querySelector (Selecionar ou pegar um item do HTML)
-    addEventListener - Sua vizinha fofoqueira (Ouvindo alguém )
+Botão -> For Clicado -> Chamar a minha Função -> Ligar o SOM
+Document = HTML
+querySelector (Selecionar ou pegar um item do HTML)
+addEventListener - Sua vizinha fofoqueira (Ouvindo alguém e te contar quando algo aconteceu)
 */
 
-let botaoSom = document.querySelector(".botao-som");
-let video = document.querySelector(".video");
-let botao = document.querySelector(".link-info");
-let modal = document.querySelector(".modal");
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
 
+// Ligar o SOM
+botaoSom.addEventListener("click", ligaSom)
 
-// Ligar e desligar o SOM com um único click.
-botaoSom.addEventListener("click", toggleSom);
-
-function toggleSom() {
-    video.muted = !video.muted; // Alterna o estado do SOM.
+function ligaSom(){
+    video.muted =  !video.muted
 }
+// ! = inverso / inverte tudo
+// Se ta mudo, ele vai tocar, se tiver tocando ele vai deixar mudo
 
-// Mostrar o MODAL.
-botao.addEventListener("click", mostrarModal);
 
-function mostrarModal() {
+// Mostrar o MODAL
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
     modal.style.display = "block"
 }
 
-// Esconde o MODAL.
-modal.addEventListener("click", esconderModal);
-
-function esconderModal() {
+function esconderModal(){
     modal.style.display = "none"
+}
+
+// Tocar o TUNDUM
+
+window.addEventListener("load", tocarAudio)
+
+
+function tocarAudio(){
+    audio.play()
 }
