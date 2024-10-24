e96d7de07f1cee4030a0eff6472f4241b0a6d364
Jefferson Formigoni <jeffersonformigoni86@gmail.com>
2024-10-24 11:50:02 -0300

    Criado botao de ativar e desativar som e modal com informações aparecendo ou nao ao clicar

--- scripts.js
+++ scripts.js
@@ -13,15 +13,31 @@
     Document = HTML
     querySelector (Selecionar ou pegar um item do HTML)
     addEventListener - Sua vizinha fofoqueira (Ouvindo alguém )
-
-
 */
 
-let botaoSom = document.querySelector(".botao-som")
-let video = document.querySelector(".video")
+let botaoSom = document.querySelector(".botao-som");
+let video = document.querySelector(".video");
+let botao = document.querySelector(".link-info");
+let modal = document.querySelector(".modal");
 
-botaoSom.addEventListener("click", ligaSom)
 
-function ligaSom() {
-    video.muted = false
+// Ligar e desligar o SOM com um único click.
+botaoSom.addEventListener("click", toggleSom);
+
+function toggleSom() {
+    video.muted = !video.muted; // Alterna o estado do SOM.
+}
+
+// Mostrar o MODAL.
+botao.addEventListener("click", mostrarModal);
+
+function mostrarModal() {
+    modal.style.display = "block"
+}
+
+// Esconde o MODAL.
+modal.addEventListener("click", esconderModal);
+
+function esconderModal() {
+    modal.style.display = "none"
 }
